1f65428ca625c1377fa350515c76c7a750cd72c5
graciousbae <graceolajide17@gmail.com>
2021-09-14 14:41:13 +0100

    updated html

---
+++ main.js
@@ -0,0 +1,51 @@
+console.log("Hi, whats up")
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons =document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal);
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+$('.btn-group').click(function(){
+    $(this).toggleClass("click");
+    $('.sidebar').toggleClass("show");
+})
+
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("box");
+
+function register(){
+    x.style.left="-400px";
+    y.style.left="50px";
+    z.style.left="110px";
+}
+function login(){
+    x.style.left="50px";
+    y.style.left="450px";
+    z.style.left="0px";
+}
